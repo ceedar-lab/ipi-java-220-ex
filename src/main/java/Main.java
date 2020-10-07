7d2647c7e5ca77d0f230c28c5749a77612bc444c
clement <clement.daroit@ipilyon.net>
2020-10-07 16:04:33 +0200

    Exercice 0 | 001 - 002 terminé

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+import com.ipiecoles.java.java220.Entreprise;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Entreprise.primeAnnuelleBase());
+    }
+}
